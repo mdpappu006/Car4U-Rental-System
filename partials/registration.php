2b11c4503e77e865340aad13d469d419cba71a76
Nazmul Hossain Pappu <mdpappu006@gmail.com>
2021-05-11 13:14:33 +0600

    backend->registration.php Error Messages.

--- partials/registration.php
+++ partials/registration.php
@@ -3,18 +3,26 @@
         $fname=trim($_POST['fullname']);
         $mobile=trim($_POST['mobileno']);
         $email=trim($_POST['emailid']);
-        $password= $_POST['password']; 
-        $sql="INSERT INTO  tblusers(FullName,EmailId,ContactNo,Password) VALUES('$fname','$email','$mobile','$password')";
-        $result= mysqli_query($db, $sql);
-        
-        if($result){	
-			$_SESSION['success'] = true;
-            header('location: ./registration.php');
-            exit;
-		}else{
-            $_SESSION['error'] = true;
-            header('location: ./registration.php');
-            exit;
+        $password = password_hash($_POST['password'], PASSWORD_BCRYPT); 
+        $conf_pass = $_POST['confirm-pass'];
+        $pass_verified = password_verify( $conf_pass, $password);
+
+        if($pass_verified){
+            $sql="INSERT INTO  tblusers(FullName,EmailId,ContactNo,Password) VALUES('$fname','$email','$mobile','$conf_pass')";
+            $result = mysqli_query($db, $sql);
+            
+            if($result){	
+                $_SESSION['success'] = true;
+                header('location: ./registration.php');
+                exit;
+            }else{
+                $_SESSION['error'] = true;
+                header('location: ./registration.php');
+                exit;
+            }
+        }else{
+            $_SESSION['Passerror'] = true;
         }
+
     }
 ?>
--- registration.php
+++ registration.php
@@ -7,7 +7,36 @@
 <!--Register-Form -->
 <div class="container">
     <div class="row">
-        <div class="registration-area m-5" id="signupform">
+
+    <!-- Error Messages -->
+    <?php if(isset($_SESSION['error'])):?>
+        <div class="mt-5 alert alert-danger text-uppercase text-center font-weight-bold" role="alert">
+            Registration Failed!
+        </div>
+    <?php unset($_SESSION["error"]);endif;?> 
+    <!-- Error Messages -->
+
+    <!-- Error Messages -->
+    <?php if(isset($_SESSION['Passerror'])):?>
+        <div class="mt-5 alert alert-danger text-uppercase text-center font-weight-bold" role="alert">
+           Confirm password doesn't match!
+        </div>
+    <?php unset($_SESSION["Passerror"]);endif;?> 
+    <!-- Error Messages -->
+
+    <!-- success Message -->    
+    <?php if(isset($_SESSION['success'])):?>
+        <div class="mt-5 alert alert-success text-uppercase text-center font-weight-bold" role="alert">
+            Registration Successful.
+        </div>
+    <?php unset($_SESSION["success"]);
+        
+    endif;?> 
+	<!-- success Message -->
+
+
+
+        <div class="registration-area " id="signupform">
             <div class="modal-content">
                 <div class="modal-header">
                     <h3 class="modal-title">Sign Up</h3>
@@ -26,7 +55,7 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <input type="email" class="form-control" placeholder="Email Address" name="emailid" required/>
+                                        <input type="text" class="form-control" placeholder="Email Address" name="emailid" required/>
                                     </div>
                                     <div class="form-group">
                                         <input type="password" class="form-control" placeholder="Password" name="password" required/>
