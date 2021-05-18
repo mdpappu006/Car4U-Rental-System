18547ad9c0f29ebbeb39fabf0bd2a37a0689d679
Nazmul Hossain Pappu <mdpappu006@gmail.com>
2021-05-18 14:26:05 +0600

    Updates SQL Files.

--- SQL/carfyou.sql
+++ SQL/carfyou.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 27, 2020 at 03:51 PM
+-- Generation Time: May 18, 2021 at 08:25 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.4.0
 
@@ -25,25 +25,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `signup_user`
+-- Table structure for table `tblusers`
 --
 
-DROP TABLE IF EXISTS `signup_user`;
-CREATE TABLE IF NOT EXISTS `signup_user` (
+DROP TABLE IF EXISTS `tblusers`;
+CREATE TABLE IF NOT EXISTS `tblusers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fullname` varchar(30) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `pass` varchar(256) NOT NULL,
+  `FullName` varchar(120) DEFAULT NULL,
+  `EmailId` varchar(100) DEFAULT NULL,
+  `Password` varchar(100) DEFAULT NULL,
+  `ContactNo` varchar(11) DEFAULT NULL,
+  `dob` varchar(120) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `City` varchar(100) DEFAULT NULL,
+  `Country` varchar(100) DEFAULT NULL,
+  `RegDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
+  UNIQUE KEY `EmailId` (`EmailId`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `signup_user`
+-- Dumping data for table `tblusers`
 --
 
-INSERT INTO `signup_user` (`id`, `fullname`, `email`, `pass`) VALUES
-(1, 'Ruth McDavid', 'ruthamcdavid@gmail.com', '$2y$10$G7DvScf.fekvm5fcoy23rO5HDUltbQ2HQqH2/mbRvrPXM7K2m5bSq');
+INSERT INTO `tblusers` (`id`, `FullName`, `EmailId`, `Password`, `ContactNo`, `dob`, `Address`, `City`, `Country`, `RegDate`, `UpdationDate`) VALUES
+(1, 'Nazmul Hossain', 'mdpappu006@gmail.com', '$2y$10$rT6VxHWSwxmIIyrEpxvWxOTsd/FoEY7KaXQo0lpj0MheHIFzp1nBS', '01775151041', NULL, NULL, NULL, NULL, '2021-05-12 12:12:02', '2021-05-12 12:12:02'),
+(2, 'Test', 'test@gmail.com', '$2y$10$Ggq22lXoZQMBifgCL2/Lz.jGQMlUgHxFmzPW74AgHzbDoQQugDZV2', '01775115041', '10/10/1995', 'West Akur Takur Para, Tangail.', 'Tangail', 'Bangladesh', '2021-05-15 04:02:56', '2021-05-15 04:02:56');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
