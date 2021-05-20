62d6978a2e44047fb3275899992fa5d2336c7093
Nazmul Hossain Pappu <mdpappu006@gmail.com>
2021-05-20 15:56:42 +0600

    admin=> post-vehical.php added UI only.

--- admin/includes/leftbar.php
+++ admin/includes/leftbar.php
@@ -10,14 +10,14 @@
 <li><a href="manage-brands.php">Manage Brands</a></li>
 </ul>
 </li>
-<!-- 
+
 <li><a href="#"><i class="fa fa-car"></i> Vehicles</a>
 					<ul>
 						<li><a href="post-avehical.php">Post a Vehicle</a></li>
 						<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
 					</ul>
 				</li>
-
+<!-- 
 <li><a href="#"><i class="fa fa-sitemap"></i> Bookings</a>
 					<ul>
 						<li><a href="new-bookings.php">New</a></li>
---
+++ admin/post-avehical.php
@@ -0,0 +1,283 @@
+<!doctype html>
+<html lang="en" class="no-js">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="theme-color" content="#3e454c">
+	
+	<title>Car Rental Portal | Admin Post Vehicle</title>
+
+	<!-- Font awesome -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<!-- Sandstone Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Bootstrap Datatables -->
+	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+	<!-- Bootstrap social button library -->
+	<link rel="stylesheet" href="css/bootstrap-social.css">
+	<!-- Bootstrap select -->
+	<link rel="stylesheet" href="css/bootstrap-select.css">
+	<!-- Bootstrap file input -->
+	<link rel="stylesheet" href="css/fileinput.min.css">
+	<!-- Awesome Bootstrap checkbox -->
+	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+	<!-- Admin Stye -->
+	<link rel="stylesheet" href="css/style.css">
+<style>
+		.errorWrap {
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #dd3d36;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+.succWrap{
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #5cb85c;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+		</style>
+
+</head>
+
+<body>
+	<?php include('includes/header.php');?>
+	<div class="ts-main-content">
+	<?php include('includes/leftbar.php');?>
+		<div class="content-wrapper">
+			<div class="container-fluid">
+
+				<div class="row">
+					<div class="col-md-12">
+					
+						<h2 class="page-title">Post A Vehicle</h2>
+
+						<div class="row">
+							<div class="col-md-12">
+								<div class="panel panel-default">
+									<div class="panel-heading">Basic Info</div>
+
+
+									<div class="panel-body">
+									
+<form method="post" class="form-horizontal" enctype="multipart/form-data">
+	<div class="form-group">
+	<label class="col-sm-2 control-label">Vehicle Title<span style="color:red">*</span></label>
+	<div class="col-sm-4">
+	<input type="text" name="vehicletitle" class="form-control" required>
+	</div>
+	<label class="col-sm-2 control-label">Select Brand<span style="color:red">*</span></label>
+	<div class="col-sm-4">
+	<select class="selectpicker" name="brandname" required>
+	<option value=""> Select </option>
+
+
+	</select>
+	</div>
+	</div>
+											
+<div class="hr-dashed"></div>
+<div class="form-group">
+<label class="col-sm-2 control-label">Vehical Overview<span style="color:red">*</span></label>
+<div class="col-sm-10">
+<textarea class="form-control" name="vehicalorcview" rows="3" required></textarea>
+</div>
+</div>
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Price Per Day(in USD)<span style="color:red">*</span></label>
+<div class="col-sm-4">
+<input type="text" name="priceperday" class="form-control" required>
+</div>
+<label class="col-sm-2 control-label">Select Fuel Type<span style="color:red">*</span></label>
+<div class="col-sm-4">
+<select class="selectpicker" name="fueltype" required>
+<option value=""> Select </option>
+
+<option value="Petrol">Petrol</option>
+<option value="Diesel">Diesel</option>
+<option value="CNG">CNG</option>
+</select>
+</div>
+</div>
+
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Model Year<span style="color:red">*</span></label>
+<div class="col-sm-4">
+<input type="text" name="modelyear" class="form-control" required>
+</div>
+<label class="col-sm-2 control-label">Seating Capacity<span style="color:red">*</span></label>
+<div class="col-sm-4">
+<input type="text" name="seatingcapacity" class="form-control" required>
+</div>
+</div>
+<div class="hr-dashed"></div>
+
+
+<div class="form-group">
+<div class="col-sm-12">
+<h4><b>Upload Images</b></h4>
+</div>
+</div>
+
+
+<div class="form-group">
+<div class="col-sm-4">
+Image 1 <span style="color:red">*</span><input type="file" name="img1" required>
+</div>
+<div class="col-sm-4">
+Image 2<span style="color:red">*</span><input type="file" name="img2" required>
+</div>
+<div class="col-sm-4">
+Image 3<span style="color:red">*</span><input type="file" name="img3" required>
+</div>
+</div>
+
+
+<div class="form-group">
+<div class="col-sm-4">
+Image 4<span style="color:red">*</span><input type="file" name="img4" required>
+</div>
+<div class="col-sm-4">
+Image 5<input type="file" name="img5">
+</div>
+
+</div>
+<div class="hr-dashed"></div>									
+</div>
+</div>
+</div>
+</div>
+							
+
+<div class="row">
+<div class="col-md-12">
+<div class="panel panel-default">
+<div class="panel-heading">Accessories</div>
+<div class="panel-body">
+
+
+<div class="form-group">
+<div class="col-sm-3">
+<div class="checkbox checkbox-inline">
+<input type="checkbox" id="airconditioner" name="airconditioner" value="1">
+<label for="airconditioner"> Air Conditioner </label>
+</div>
+</div>
+<div class="col-sm-3">
+<div class="checkbox checkbox-inline">
+<input type="checkbox" id="powerdoorlocks" name="powerdoorlocks" value="1">
+<label for="powerdoorlocks"> Power Door Locks </label>
+</div></div>
+<div class="col-sm-3">
+<div class="checkbox checkbox-inline">
+<input type="checkbox" id="antilockbrakingsys" name="antilockbrakingsys" value="1">
+<label for="antilockbrakingsys"> AntiLock Braking System </label>
+</div></div>
+<div class="checkbox checkbox-inline">
+<input type="checkbox" id="brakeassist" name="brakeassist" value="1">
+<label for="brakeassist"> Brake Assist </label>
+</div>
+</div>
+
+
+
+<div class="form-group">
+<div class="col-sm-3">
+<div class="checkbox checkbox-inline">
+<input type="checkbox" id="powersteering" name="powersteering" value="1">
+<input type="checkbox" id="powersteering" name="powersteering" value="1">
+<label for="inlineCheckbox5"> Power Steering </label>
+</div>
+</div>
+<div class="col-sm-3">
+<div class="checkbox checkbox-inline">
+<input type="checkbox" id="driverairbag" name="driverairbag" value="1">
+<label for="driverairbag">Driver Airbag</label>
+</div>
+</div>
+<div class="col-sm-3">
+<div class="checkbox checkbox-inline">
+<input type="checkbox" id="passengerairbag" name="passengerairbag" value="1">
+<label for="passengerairbag"> Passenger Airbag </label>
+</div></div>
+<div class="checkbox checkbox-inline">
+<input type="checkbox" id="powerwindow" name="powerwindow" value="1">
+<label for="powerwindow"> Power Windows </label>
+</div>
+</div>
+
+
+<div class="form-group">
+<div class="col-sm-3">
+<div class="checkbox checkbox-inline">
+<input type="checkbox" id="cdplayer" name="cdplayer" value="1">
+<label for="cdplayer"> CD Player </label>
+</div>
+</div>
+<div class="col-sm-3">
+<div class="checkbox h checkbox-inline">
+<input type="checkbox" id="centrallocking" name="centrallocking" value="1">
+<label for="centrallocking">Central Locking</label>
+</div></div>
+<div class="col-sm-3">
+<div class="checkbox checkbox-inline">
+<input type="checkbox" id="crashcensor" name="crashcensor" value="1">
+<label for="crashcensor"> Crash Sensor </label>
+</div></div>
+<div class="col-sm-3">
+<div class="checkbox checkbox-inline">
+<input type="checkbox" id="leatherseats" name="leatherseats" value="1">
+<label for="leatherseats"> Leather Seats </label>
+</div>
+</div>
+</div>
+
+
+
+
+											<div class="form-group">
+												<div class="col-sm-8 col-sm-offset-2">
+													<button class="btn btn-default" type="reset">Cancel</button>
+													<button class="btn btn-primary" name="submit" type="submit">Save changes</button>
+												</div>
+											</div>
+
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+					
+
+					</div>
+				</div>
+				
+			
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Loading Scripts -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap-select.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+	<script src="js/dataTables.bootstrap.min.js"></script>
+	<script src="js/Chart.min.js"></script>
+	<script src="js/fileinput.js"></script>
+	<script src="js/chartData.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
