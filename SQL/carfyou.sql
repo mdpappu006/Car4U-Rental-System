cba2a1f5a5e601448dd9a02d9c1538c6b7f0baf2
Nazmul Hossain Pappu <mdpappu006@gmail.com>
2021-05-21 10:34:38 +0600

    SQL file updates.

--- SQL/carfyou.sql
+++ SQL/carfyou.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 19, 2021 at 05:46 PM
+-- Generation Time: May 21, 2021 at 04:33 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.4.0
 
@@ -42,7 +42,31 @@ CREATE TABLE IF NOT EXISTS `admin` (
 --
 
 INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
-(1, 'admin', '$2y$10$gfLavrqIanM8Z7ADv4Gkc.CxG76x4vjtLKkO6FHuPSvmzkWe/Nofm', '2021-05-19 15:47:16');
+(1, 'admin', '$2y$10$QaDD71HmPOir8gnr4L2PpuHWNDIjZhokAphmm23hKuq2CzTjAOkce', '2021-05-19 15:47:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblbrands`
+--
+
+DROP TABLE IF EXISTS `tblbrands`;
+CREATE TABLE IF NOT EXISTS `tblbrands` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `BrandName` varchar(120) NOT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblbrands`
+--
+
+INSERT INTO `tblbrands` (`id`, `BrandName`, `CreationDate`, `UpdationDate`) VALUES
+(1, 'Maruti', '2021-05-20 06:53:12', NULL),
+(2, 'maruti', '2021-05-20 06:54:10', NULL),
+(3, 'BMW', '2021-05-20 06:54:45', NULL);
 
 -- --------------------------------------------------------
 
@@ -74,6 +98,44 @@ CREATE TABLE IF NOT EXISTS `tblusers` (
 INSERT INTO `tblusers` (`id`, `FullName`, `EmailId`, `Password`, `ContactNo`, `dob`, `Address`, `City`, `Country`, `RegDate`, `UpdationDate`) VALUES
 (1, 'Nazmul Hossain', 'mdpappu006@gmail.com', '$2y$10$rT6VxHWSwxmIIyrEpxvWxOTsd/FoEY7KaXQo0lpj0MheHIFzp1nBS', '01775151041', NULL, NULL, NULL, NULL, '2021-05-12 12:12:02', '2021-05-12 12:12:02'),
 (2, 'Test', 'test@gmail.com', '$2y$10$Ggq22lXoZQMBifgCL2/Lz.jGQMlUgHxFmzPW74AgHzbDoQQugDZV2', '01775115041', '10/10/1995', 'West Akur Takur Para, Tangail.', 'Tangail', 'Bangladesh', '2021-05-15 04:02:56', '2021-05-15 04:02:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblvehicles`
+--
+
+DROP TABLE IF EXISTS `tblvehicles`;
+CREATE TABLE IF NOT EXISTS `tblvehicles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `VehiclesTitle` varchar(150) DEFAULT NULL,
+  `VehiclesBrand` int(11) DEFAULT NULL,
+  `VehiclesOverview` longtext DEFAULT NULL,
+  `PricePerDay` int(11) DEFAULT NULL,
+  `FuelType` varchar(100) DEFAULT NULL,
+  `ModelYear` int(6) DEFAULT NULL,
+  `SeatingCapacity` int(11) DEFAULT NULL,
+  `Vimage1` varchar(120) DEFAULT NULL,
+  `Vimage2` varchar(120) DEFAULT NULL,
+  `Vimage3` varchar(120) DEFAULT NULL,
+  `Vimage4` varchar(120) DEFAULT NULL,
+  `Vimage5` varchar(120) DEFAULT NULL,
+  `AirConditioner` int(11) DEFAULT NULL,
+  `PowerDoorLocks` int(11) DEFAULT NULL,
+  `AntiLockBrakingSystem` int(11) DEFAULT NULL,
+  `BrakeAssist` int(11) DEFAULT NULL,
+  `PowerSteering` int(11) DEFAULT NULL,
+  `DriverAirbag` int(11) DEFAULT NULL,
+  `PassengerAirbag` int(11) DEFAULT NULL,
+  `PowerWindows` int(11) DEFAULT NULL,
+  `CDPlayer` int(11) DEFAULT NULL,
+  `CentralLocking` int(11) DEFAULT NULL,
+  `CrashSensor` int(11) DEFAULT NULL,
+  `LeatherSeats` int(11) DEFAULT NULL,
+  `RegDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
